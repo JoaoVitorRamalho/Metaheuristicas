9a0b898a43a1a0aea9cf568ca22719f1e8b423a1
JOAO VITOR RAMALHO GONCALVES <0048613@IFMG.EDU.BR>
2025-09-26 10:33:07 -0300

    Adicionando aula 1

---
+++ 01-operacoes-aritmeticas.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+soma = a+b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+divisao_inteira = a//b
+resto = a%b
+exponenciacao = a ** b
+
+print(f'A soma é: {soma}')
+print(f'A subtracao é: {subtracao}')
+print(f'A multiplicacao é: {multiplicacao}')
+print(f'A divisao é: {divisao}')
+print(f'A divisao_inteira é: {divisao_inteira}')
+print(f'A resto é: {resto}')
+print(f'A exponenciacao é: {exponenciacao}')
+
---
+++ 02-tipos-de-dados.py
@@ -0,0 +1,15 @@
+nome = 'Joao' #string
+sala = 3 #inteiro
+altura = 1.90
+is_student = True #boolean
+notas = [9.0,10,9.5] #lista
+numeros = (1,2,3) #tuplas
+pessoa = {'nome':'Joao', 'disciplina':'Metaheuristicas'} #Dicionario
+cores = {'vermelho', 'azul', 'verde'} #Conjunto
+
+print('Nome: '+ nome + ' a sala: '+str(sala)+' altura: '+str(altura) + ' é estudante: '+str(is_student))
+
+print(f'Notas: {notas}')
+print(f'Numeros: {numeros}')
+print(f'Pessoa: {pessoa}')
+print(f'Cores: {cores}')
